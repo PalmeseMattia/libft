8d2a24a323ec1c3ce90c00362e67706585c6aae0
PalmeseMattia <palmesedavidemattia@gmail.com>
2023-10-15 18:58:30 +0200

    fix memmove

--- libft.c
+++ libft.c
@@ -61,10 +61,25 @@ void *ft_memccpy(void *dest, const void *src, int c, size_t n)
  */
 void *ft_memmove(void *dest, const void *src, size_t n)
 {
-    unsigned char *srccpy = (unsigned char *)malloc(sizeof(unsigned char) * n);
-    ft_memcpy(srccpy, src, n);
-    ft_memcpy(dest, srccpy, n);
-    return (dest);
+        uint8_t* from = (uint8_t*) src;
+        uint8_t* to = (uint8_t*) dest;
+
+        if (from == to || n == 0)
+                return dest;
+        if (to > from && to-from < (int)n) {
+                int i;
+                for(i=n-1; i>=0; i--)
+                        to[i] = from[i];
+                return dest;
+        }
+        if (from > to && from-to < (int)n) {
+                size_t i;
+                for(i=0; i<n; i++)
+                        to[i] = from[i];
+                return dest;
+        }
+        ft_memcpy(dest, src, n);
+        return dest;
 }
 
 /*
