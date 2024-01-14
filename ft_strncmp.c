f8b349505698fb65a24553576d816f73e0c6e4f6
PalmeseMattia <palmesedavidemattia@gmail.com>
2024-01-14 18:55:41 +0100

    bug fix

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -13,10 +13,14 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	while ((*s1 && (*s1 == *s2)) && n--)
+	if (n == 0){
+		return (0);
+	}
+	while ((*s1 && *s2 && (*s1 == *s2)) && n > 1)
 	{
 		s1++;
 		s2++;
+		n--;
 	}
 	return (*(const unsigned char *) s1 - *(const unsigned char *) s2);
 }
