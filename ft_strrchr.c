24392c561ad18a029f7be96f869495cf0912e887
PalmeseMattia <palmesedavidemattia@gmail.com>
2024-01-19 21:31:58 +0100

    added norm

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -1,18 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpalmese <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/19 21:30:23 by dpalmese          #+#    #+#             */
+/*   Updated: 2024/01/19 21:30:26 by dpalmese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "libft.h"
 
 /*
  * Like strchr() but in reverse
  */
-char    *ft_strrchr(const char *s, int c)
+char	*ft_strrchr(const char *s, int c)
 {
-    int offset;
+	int	offset;
 
-    offset = ft_strlen(s);
-    while (offset >= 0)
-    {
-        if ((unsigned char)*(s + offset    ) == (unsigned char)c)
-            return ((char *)(s + offset));
-        offset--;
-    }
-    return (0);
+	offset = ft_strlen(s);
+	while (offset >= 0)
+	{
+		if ((unsigned char)*(s + offset) == (unsigned char)c)
+			return ((char *)(s + offset));
+		offset--;
+	}
+	return (0);
 }
