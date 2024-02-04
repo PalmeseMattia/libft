b7a533a6b29045b3ebfd183195697c1f216e9ca7
PalmeseMattia <palmesedavidemattia@gmail.com>
2024-02-04 18:12:19 +0100

    resolved conflict

--- ft_putnbrbase_fd.c
+++ ft_putnbrbase_fd.c
@@ -1,10 +1,6 @@
 #include "libft.h"
 
-<<<<<<< HEAD
-int	ft_putnbrbase_fd(int number, int base, int fd)
-=======
 int	ft_putnbrbase_fd(long number, int base, int fd)
->>>>>>> 7a8b37fe252a0cfc82562120fef47fc2add8f926
 {
 	char *digits;
 	int result;
@@ -12,15 +8,6 @@ int	ft_putnbrbase_fd(long number, int base, int fd)
 	digits = "0123456789ABCDEF";
 	if (fd < 0)
 		return (0);
-<<<<<<< HEAD
-	if (number < 0)
-	{
-		result += ft_putchar_fd('-', fd);
-		number = -number;
-	}
-	if (base < 2 || base > 16)
-		return (0);
-=======
 	if (base < 2 || base > 16)
 		return (0);
 	if (number < 0)
@@ -28,7 +15,6 @@ int	ft_putnbrbase_fd(long number, int base, int fd)
 		ft_putchar_fd('-', fd);
 		number = -number;
 	}
->>>>>>> 7a8b37fe252a0cfc82562120fef47fc2add8f926
 	if (number < base)
 	{
 		ft_putchar_fd(digits[number], fd);
