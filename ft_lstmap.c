20ca1f49fd20c932e023ee0085a9a2451da09b4c
PalmeseMattia <palmesedavidemattia@gmail.com>
2024-10-06 14:49:00 +0200

    norminette

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,29 +6,29 @@
 /*   By: dpalmese <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 16:21:44 by dpalmese          #+#    #+#             */
-/*   Updated: 2024/01/21 16:21:47 by dpalmese         ###   ########.fr       */
+/*   Updated: 2024/10/06 12:30:31 by dpalmese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lmap(t_list *l, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
 	t_list	*tmp;
 
-	if (!lst || !f || !del)
+	if (!l || !f || !del)
 		return (0);
 	new = NULL;
-	while (lst)
+	while (l)
 	{
-		tmp = ft_lstnew((*f)(lst->content));
+		tmp = ft_lstnew((*f)(l->content));
 		if (!tmp)
 		{
 			ft_lstclear(&new, del);
 			return (NULL);
 		}
 		ft_lstadd_back(&new, tmp);
-		lst = lst->next;
+		l = l->next;
 	}
 	return (new);
 }
